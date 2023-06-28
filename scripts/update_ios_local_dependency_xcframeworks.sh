4affd3e501600178a76015af89daffb6aed291e3
hvdwolf <hvdwolf@gmail.com>
2023-06-28 20:13:13 +0200

    add forgotten files

---
+++ scripts/update_ios_local_dependency_xcframeworks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export BASEDIR="$(pwd)"
+
+cd ${BASEDIR}/../ios/test-app-local-dependency || exit 1
+rm -rf *.framework || exit 1
+rm -rf *.xcframework || exit 1
+find ${BASEDIR}/../../ffmpeg-kit/prebuilt/bundle-apple-xcframework-ios -name "*.xcframework" -exec cp -R {} . \; || exit 1
